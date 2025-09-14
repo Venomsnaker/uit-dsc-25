47d3fbec8376c717bd481c134b076b91a7071374
Venomsnaker <qhuy.bui.developer@gmail.com>
2025-09-14 09:05:55 +0700

    SelfCheck: Prompts

--- selfcheck.py
+++ selfcheck.py
@@ -15,6 +15,12 @@ class SelfCheckPrompt:
             self.intrinsic_prompt_template = f.read()
         with open(extrinsic_prompt_path) as f:
             self.extrinsic_prompt_template = f.read()
+        self.output_dict = {
+            'yes': 1,
+            'n/a': 0.5,
+            'no': 0
+        }
+        self.not_defined = 'n/a'
         
     def _generate(self, prompt: str, max_new_tokens:int):
         messages = [
@@ -30,5 +36,24 @@ class SelfCheckPrompt:
         output_ids = generated_ids[0][len(model_inputs.input_ids[0]) :].tolist()
         content = self.tokenizer.decode(output_ids, skip_special_tokens=True)
         return content
+    
+    def _postprocess(self, response):
+        response = response.lower().strip()
+
+        for output in self.output_dicts:
+            if output in response:
+                response = output
+        if response not in self.output_dicts.keys():
+            response = 'n/a'
+        return self.output_dict[response]
+
+    def predict_intrinsic(self, context: str, sentence: str):
+        prompt = self.intrinsic_prompt_template.format(context=context, sentence=sentence)
+        return self._postprocess(self._generate(prompt, max_new_tokens=4))
+    
+    def predict_extrinsic(self, context: str, sentence: str):
+        prompt = self.extrinsic_prompt_template.format(context=context, sentence=sentence)
+        return self._postprocess(self._generate(prompt, max_new_tokens=4))
+
 
     
